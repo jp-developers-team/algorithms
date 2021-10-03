027a2d860c56313b1a16dce0617066fbf1232e67
Helington007 <46464164+helington007@users.noreply.github.com>
2021-10-03 17:27:58 -0300

    adicionando novos algoritmos básicos m pythonn

---
+++ python/algorithms/area_perimetro.py
@@ -0,0 +1,37 @@
+# Algorítmo...
+# Calculando o valor de uma área e do perímetro
+# será informado o valor do lado de um quadrado e logo apóe, será calculado o valor da área e o valor do perímetro
+# Objetivo é calcular uma expressão muito usada no dia a dia de muitos, [Lado, Área e Perímetro], além, do [while] que é uma estrutura de repetição
+
+# Atribuindo o valor default
+option = "s"
+
+# Laço de repetição, enquanto o usuário escolher "SIM", ocorrerá a repetição
+while (option == "s"):
+
+    # Estrutura condicional, se o usuário escolher "SIM", vai ser dado o início do programa
+    if (option == "s"):
+
+        # Tipando as variáveis para o tipo número inteiro
+        lado = int(input("Digite o valor corresondente ao lado\n"))
+
+        # Exibindo o resultado na tela
+        print('O valor correspondente ao perímetro é {} e o valor corresondente a área é {}!\n'.format((lado*4), (lado*lado)))
+
+    # Fim da estrutura condicional, se o usuário escolher "NÃO", o programa será encerrado
+    else:
+
+        # Imprimindo na tela o resultado
+        print('Você escolheu SAIR!\n')
+
+    # Permite o usuário reiniciar automaticamente o LOOP, ou seja, o laço de repetição
+    option = input("Deseja rodar o código novamente? \nPressione [s] para SIM \nOu \nPressione [n] para NÃO\n")
+
+# Verifica se o usuário optou por sair, ao inserir um valor diferente de "s"
+if (option != "s"):
+
+    # Imprimindo na tela o resultado
+    print('Você escolheu SAIR!\n')
+
+    # Encerra o programa
+    exit()
--- python/algorithms/maior_numero.py
+++ python/algorithms/maior_numero.py
@@ -1,3 +1,11 @@
+# Algorítmo...
+# Calculando qual é o maior número
+# O usuário informará um rimeiro númeo inteirioo qualquer
+# Logo após informará um segundo númeo inteirioo qualquer
+# Será realizado uma comparação, entre os dois valores inseridos
+# Será apresentado o resultado de qual dos números inseridos é maior
+# Objetivo é trabalhar estrutura condicional, [if, else if, else], além, do [while] que é uma estrutura de repetição
+
 # Atribuindo o valor default
 option = "s"
 
@@ -13,23 +21,36 @@ while (option == "s"):
         
         # Verificamos se o primeiro número é maior do que o segundo número
         if (number1 > number2):
-            #print('A SUBTRAÇÃO entre {} e {} é = {}\n'.format(number1, number2, result))
-            
+
+            # Imprimindo na tela o resultado
             print('O primeiro número {} é maior do que o segundo número {}!\n'.format(number1, number2))
         
         # Verificamos se o segundo número é menor do que o primeiro número
         elif (number1 < number2):
-            
+
+            # Imprimindo na tela o resultado
             print('O segundo número {} é maior do que o primeiro número {}!\n'.format(number2, number1))
             
         # Verificamos se o primeiro número é igual ao segundo número            
         else:
+
+            # Imprimindo na tela o resultado
             print('O primeiro número {} e o segundo número {} são iguais!\n'.format(number1, number2))
                 
     # Fim da estrutura condicional, se o usuário escolher "NÃO", o programa será encerrado
     else:
+
         # Imprimindo na tela o resultado
         print('Você escolheu SAIR!\n')
 
     # Permite o usuário reiniciar automaticamente o LOOP, ou seja, o laço de repetição
     option = input("Deseja rodar o código novamente? \nPressione [s] para SIM \nOu \nPressione [n] para NÃO\n")
+
+# Verifica se o usuário optou por sair, ao inserir um valor diferente de "s"
+if (option != "s"):
+
+    # Imprimindo na tela o resultado
+    print('Você escolheu SAIR!\n')
+
+    # Encerra o programa
+    exit()
